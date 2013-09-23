2530526b11c5a87d08498b3260ab226b8919d96e
marymiller <marymiller18@ymail.com>
2013-09-22 20:50:15 -0400

    Fixed small bugs with #3 and #8.

--- QueriesHomework2.sql
+++ QueriesHomework2.sql
@@ -6,8 +6,8 @@
 select city
 from agents
 where aid in (select aid
-	  from orders
-	  where cid = 'c002')	
+	      from orders
+	      where cid = 'c002')	
 -- 2. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto.
 -- (This is not the same as asking for pids of products ordered by a customer in Kyoto.)
 select distinct pid
@@ -21,9 +21,9 @@ order by pid
 -- 3. Find the cids and name of customers who never placed an order through agent a03
 select cid, name
 from customers 
-where cid in (select cid
+where cid not in (select cid
 	      from orders 
-	      where aid != 'a03')
+	      where aid = 'a03')
 -- 4. Get the cids and names of customers who ordered both product p01 and p07.
 select cid, name
 from customers
@@ -50,7 +50,8 @@ where cid in (select cid
 -- 7. Find all customers who have the same discount as that of any customers in Dallas or Kyoto.
 
 -- 8. Get the IDs of customers who did not place an order any orders through agent a03. 
-select distinct cid
-from orders
-where aid != 'a03'
-order by cid
+select cid
+from customers 
+where cid not in (select cid
+	      from orders 
+	      where aid = 'a03')
